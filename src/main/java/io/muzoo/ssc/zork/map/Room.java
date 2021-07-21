ee2422aad02944067062c57ea31c37f2d8d515a1
harinkhanijou <simarkhanijou@gmail.com>
2021-07-21 13:40:20 +0700

    MAde map but didnt put monster and item into it yet

--- src/main/java/io/muzoo/ssc/zork/map/Room.java
+++ src/main/java/io/muzoo/ssc/zork/map/Room.java
@@ -7,32 +7,20 @@ public class Room
     private String description;
     private HashMap exits;
 
-    /**
-     * Create a room described "description". Initially, it has
-     * no exits. "description" is something like "a kitchen" or
-     * "an open court yard".
-     * @param description The room's description.
-     */
+
     public Room(String description)
     {
         this.description = description;
         exits=new HashMap();
     }
 
-    /**
-     * Define the exits of this room.  Every direction either leads
-     * to another room or is null (no exit there).
 
-     */
     public void setExit(String direction,Room neighbor)
     {
         exits.put(direction,neighbor);
     }
 
 
-    /**
-     * @return The description of the room.
-     */
     public String getDescription()
     {
         return description;
